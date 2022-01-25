be727eb0c1770e80d057d24ab8ab5424d585732a
emma <karlssonemma93@gmail.com>
2022-01-25 16:33:10 +0100

    added about section and pic section

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ export default function Home() {
     width: 100%;
     height: max-content;
     padding: ${props => props.theme.space[5]};
+
     background-color: green;
     font-family: ${props => props.theme.fonts.main};
   `;
@@ -22,11 +23,64 @@ export default function Home() {
     margin-bottom: ${props => props.theme.space[2]};
   `;
 
+  const Subtitle = styled.h2`
+    margin-bottom: ${props => props.theme.space[2]};
+
+    font-size: ${props => props.theme.fontSizes.l};
+    font-family: ${props => props.theme.fonts.main};
+  `;
+
   const CtaLink = styled.a`
+    display: block;
     color: ${props => props.theme.colors.orange};
     cursor: pointer;
+    margin-top: ${props => props.theme.space[2]};
     :hover {
       text-decoration: underline;
+    }
+  `;
+
+  const Arrow = styled.img`
+    width: 20px;
+    height: 20px;
+    margin-right: 10px;
+    position: relative;
+    top: 2px;
+  `;
+
+  const AboutText = styled.p`
+    font-family: ${props => props.theme.fonts.main};
+    line-height: 180%;
+  `;
+
+  const GridContainer = styled.article`
+    display: grid;
+    background-color: green;
+    width: 100vw;
+    gap: 50px;
+
+    @media screen and (min-width: ${props => props.theme.breakpoints[0]}) {
+      grid-template-columns: 1fr 1fr;
+      grid-template-rows: 1fr 1fr;
+      height: 400px;
+    }
+
+    @media screen and (min-width: ${props => props.theme.breakpoints[2]}) {
+      //bredden på griden vid større strl
+      width: 75%;
+    }
+  `;
+  
+  const GridImg = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+
+    :first-of-type {
+      @media screen and (min-width: ${props => props.theme.breakpoints[0]}) {
+        grid-row-start: 1;
+        grid-row-end: span 2;
+      }
     }
   `;
 
@@ -38,8 +92,30 @@ export default function Home() {
         <LandingBlock>
           <Heading>Vi printar logos.</Heading>
           <p>Structured Query Language är ett standardiserat programspråk för att hämta och modifiera data i en relationsdatabas. SQL uttalas bokstav för bokstav eller ibland "s'ikuell" som i engelskans "sequel"</p>
-          <CtaLink><Image src='/arrow.png' width={200} height={200} />Vill ni veta mer? Kontakta oss här</CtaLink>
+          <CtaLink>
+            <Arrow src='/arrow.png' />
+            <p style={{display: 'inline-block', marginLeft: '0.5em'}}>Vill ni veta mer? Kontakta oss här</p>
+          </CtaLink>
         </LandingBlock>
+      </PageSection>
+
+      <PageSection style={{flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start'}}>
+        <Subtitle>Subtitle</Subtitle>
+        <AboutText>Bacon ipsum dolor amet jowl bacon chislic corned beef ham hock, short loin alcatra rump shank bresaola ham burgdoggen. Cow short loin tenderloin short ribs sausage leberkas pork loin. Shoulder tail rump pig tenderloin t-bone tongue ham ribeye ball tip cupim boudin hamburger. Turducken drumstick boudin, doner ribeye sausage sirloin tail salami pork. Leberkas andouille chuck hamburger short ribs. Pork chop picanha meatloaf biltong ribeye prosciutto brisket kevin. Tri-tip bacon pork loin filet mignon frankfurter burgdoggen, strip steak brisket picanha buffalo beef ribs tenderloin venison.</AboutText>
+      </PageSection>
+
+      <PageSection>
+        <GridContainer>
+          <GridImg
+            src='/markus-spiske-hqCEQTc5gZA-unsplash.jpg'
+          />
+          <GridImg
+            src='/bank-phrom-Tzm3Oyu_6sk-unsplash.jpg'
+          />
+          <GridImg
+            src='/mika-baumeister-PtabTe6iJ_8-unsplash.jpg'
+          />
+        </GridContainer>
       </PageSection>
     </>
   )
